5551c85b35f165e8d0788e0ca04336ced85c740c
oguzerol <oguzerol91@gmail.com>
2021-01-09 17:12:03 +0300

    Add supertest and jest to the project.

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,20 @@
-const env = process.env.NODE_ENV || "development";
-require("dotenv").config({ path: `./config/enviroment/.development` });
-
 module.exports = {
   development: {
+    client: "pg",
+    connection: {
+      host: "postgres",
+      database: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+  test: {
     client: "pg",
     connection: {
       host: "postgres",
