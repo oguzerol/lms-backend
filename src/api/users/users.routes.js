3709d77676cf2ba4081f9cf181c7cd177f9e3b8b
oguzerol <oguzerol91@gmail.com>
2021-01-23 14:21:55 +0300

    Add await to the project.

--- src/api/users/users.routes.js
+++ src/api/users/users.routes.js
@@ -1,10 +1,18 @@
+const to = require("await-to-js").default;
 const User = require("./users.model");
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const users = await User.query();
+  [err, users] = await to(User.query());
+  // TODO: Create generic error handler
+  if (err)
+    return res.status(400).json({
+      status: false,
+      message: "Kullanıcı bilgileri güncellenirken bir hata oluştu.",
+      stack: err.message,
+    });
   res.json(users);
 });
 
