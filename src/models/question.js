22de53304dc7ec412fc6d10687e6f26f819ac9ec
oguzerol <oguzerol91@gmail.com>
2021-02-03 00:23:15 +0300

    Add question & answer model.

--- src/models/question.js
+++ src/models/question.js
@@ -6,4 +6,36 @@ export default class Question extends Model {
   static get tableName() {
     return tableNames.answers;
   }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["type", "info", "content"],
+      properties: {
+        id: { type: "integer" },
+        type: { type: "string" },
+        info: { type: "string" },
+        content: { type: "string" },
+        exam_id: { type: "integer" },
+        created_at: { type: "timestamp" },
+        updated_at: { type: "timestamp" },
+        deleted_at: { type: ["timestamp", "null"] },
+      },
+    };
+  }
+
+  static get relationMappings() {
+    const Exam = require("/exam");
+
+    return {
+      exam: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Exam,
+        join: {
+          from: `${tableNames.questions}.exam_id`,
+          to: `${tableNames.exams}.id`,
+        },
+      },
+    };
+  }
 }
