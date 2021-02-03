fc0096e9877e544f5e958d98234b4a1392be7c14
oguzerol <oguzerol91@gmail.com>
2021-02-03 23:39:14 +0300

    Add authorization middleware.

--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const account = require("./account/account.routes");
-const exams = require("./exams/exams.routes");
+import express from "express";
+import account from "./account/account.routes";
+import exams from "./exams/exams.routes";
+import authorization from "../../utils/authorization";
 
 const router = express.Router();
 
 router.use("/account", account);
-router.use("/exams", exams);
+router.use("/exams", authorization, exams);
 
 module.exports = router;
